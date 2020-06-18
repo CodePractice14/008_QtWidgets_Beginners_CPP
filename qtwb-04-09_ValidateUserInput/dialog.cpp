f4eaa425a61df4003bba070f229979235c982c26
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-06-18 18:45:18 +0300

    Validating user input (uses RegEx)

---
+++ qtwb-04-09_ValidateUserInput/dialog.cpp
@@ -0,0 +1,58 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    QRegularExpression rx("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b",QRegularExpression::CaseInsensitiveOption);
+
+    ui->txtEmail->setValidator(new QRegularExpressionValidator(rx,this));
+
+    connect(ui->txtEmail,&QLineEdit::textChanged,this,&Dialog::checkInput);
+
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+
+void Dialog::on_buttonBox_accepted()
+{
+    if(ui->txtEmail->hasAcceptableInput())
+    {
+        QMessageBox::information(this,"Email", ui->txtEmail->text());
+        accept();
+    }
+    else
+    {
+        QMessageBox::critical(this,"Email", "Not valid!");
+    }
+
+
+}
+
+void Dialog::on_buttonBox_rejected()
+{
+    reject();
+}
+
+void Dialog::checkInput()
+{
+    if(ui->txtEmail->hasAcceptableInput())
+    {
+        ui->txtEmail->setStyleSheet("QLineEdit {color: black;}");
+    }
+    else
+    {
+        ui->txtEmail->setStyleSheet("QLineEdit {color: red;}");
+    }
+}
+
+
+
+
---
+++ qtwb-04-09_ValidateUserInput/dialog.h
@@ -0,0 +1,31 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QMessageBox>
+#include <QRegularExpression>
+#include <QRegularExpressionValidator>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Dialog; }
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_buttonBox_rejected();
+
+    void checkInput();
+
+private:
+    Ui::Dialog *ui;
+};
+#endif // DIALOG_H
