b34394d8fd224573be57229453cec919df4f9dd4
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-06-19 15:31:39 +0300

    Showing other dialogs (with/without parent)

---
+++ qtwb-06-01_ShowingOtherDialogs/dialog.cpp
@@ -0,0 +1,30 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_btnWithParent_clicked()
+{
+    //With Parent
+    Dialog2 *dialog = new Dialog2(this);
+
+    dialog->show();
+}
+
+void Dialog::on_btnWithoutParent_clicked()
+{
+    //Without Parent
+    Dialog2 *dialog = new Dialog2(nullptr);
+    dialog->show();
+}
