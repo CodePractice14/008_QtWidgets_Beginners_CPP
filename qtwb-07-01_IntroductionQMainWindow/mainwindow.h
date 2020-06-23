8d2a9df02e54d57ad3baeb8d3fc6b64ebf772964
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-06-23 15:38:37 +0300

    Introduction to QMainWindow

---
+++ qtwb-07-01_IntroductionQMainWindow/mainwindow.h
@@ -0,0 +1,43 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QtDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionSelectAll_triggered();
+
+private:
+    Ui::MainWindow *ui;
+
+    QString m_filename;
+};
+#endif // MAINWINDOW_H
